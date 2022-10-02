5bf454ae321730e0a6b85147e31a106b54346058
BrianDYKim <18k7102dy@naver.com>
2022-10-02 23:47:02 +0900

    [UPDATE] (domain) ShopItem 도메인 수정 완료

--- domain/src/main/kotlin/team/bakkas/domain/shopItem/persist/ShopItem.kt
+++ domain/src/main/kotlin/team/bakkas/domain/shopItem/persist/ShopItem.kt
@@ -20,13 +20,18 @@ class ShopItem(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0L,
     var name: String = "",
-    @Embedded var priceInfo: PriceInfo = PriceInfo(),
+    @Embedded
+    var priceInfo: PriceInfo = PriceInfo(),
+    @Column(name = "item_image_url")
     var itemImage: String = "",
-    var isOnSale: Boolean = false,
+    @Column(name = "is_on_sale") var isOnSale: Boolean = false,
     var description: String = "",
+    @Column(name = "item_quantity")
     var itemQuantity: Int = 0,
+    @Column(name = "discounted_at")
     var discountedAt: LocalDateTime? = null,
     var category: Category = Category.ITEM,
+    @Column(name = "detail_category")
     var detailCategory: DetailCategory = DetailCategory.DETAIL_ITEM
 ) : BaseTimeEntity() {
 }
