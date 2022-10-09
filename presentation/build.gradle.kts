c4160617f1853bd0dbf394f791f8031744d7f30c
BrianDYKim <18k7102dy@naver.com>
2022-10-10 00:42:46 +0900

    [ADD] (presentation) Add the Jenkins pipeline

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,4 +1,18 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+val deployVersion = "0.0.1"
+
 dependencies {
     // Dependency from domain
     api(project(":domain"))
+}
+
+tasks {
+    named<Jar>("jar") {
+        enabled = false
+    }
+
+    named<BootJar>("bootJar") {
+        archiveName = "withmarket-item-$deployVersion.jar"
+    }
 }
