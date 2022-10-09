7ebcaa82d8518f3b179776b9349237071e7fbcf9
BrianDYKim <18k7102dy@naver.com>
2022-10-10 01:25:46 +0900

    [DELETE] (infrastructure) Infrastructure layer 삭제

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,18 +1,4 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
-val deployVersion = "0.0.1"
-
 dependencies {
     // Dependency from domain
     api(project(":domain"))
-}
-
-tasks {
-    named<Jar>("jar") {
-        enabled = false
-    }
-
-    named<BootJar>("bootJar") {
-        archiveName = "withmarket-item-$deployVersion.jar"
-    }
 }
