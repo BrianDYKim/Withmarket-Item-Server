a72a46073c20b887d90c7355739278dd310471bc
BrianDYKim <18k7102dy@naver.com>
2022-10-11 19:56:47 +0900

    [UPDATE] (ROOT) 테스트 제외 코드 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,11 +128,4 @@ tasks {
     named<BootJar>("bootJar") {
         enabled = false
     }
-
-    named<Test>("test") {
-        // grpcInterface, common, domain에 대해선 테스트 목록에서 제외시킨다
-        excludeTestModuleList.forEach {
-            exclude("${groupName}.${it}/**")
-        }
-    }
 }
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -2,6 +2,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val deployVersion = "0.0.1"
 
+val groupName = "team.bakkas"
+val excludeTestModuleList = listOf("common", "domain", "grpcinterface")
+
 dependencies {
     // Dependency from domain
     api(project(":domain"))
@@ -14,5 +17,12 @@ tasks {
 
     named<BootJar>("bootJar") {
         archiveName = "withmarket-item-$deployVersion.jar"
+    }
+
+    named<Test>("test") {
+        // grpcInterface, common, domain에 대해선 테스트 목록에서 제외시킨다
+        excludeTestModuleList.forEach {
+            exclude("${groupName}.${it}/**")
+        }
     }
 }
